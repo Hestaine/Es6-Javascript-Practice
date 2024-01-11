9556b8153da8e3f849209ca0ef7c186957bfa0b2
Hestaine <ariffhestaine@gmail.com>
2024-01-11 09:16:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const input = document.getElementById("inputnumber");
+const displayValue = document.querySelector(".numberoddoreven");
+numValue = "null";
+
+
+input.addEventListener("input", (e) => {
+if (input.value % 2 == 1) {
+    numValue = "0dd"
+}
+else if (input.value % 2 == 0) {
+    numValue = "even";
+}
+    displayValue.innerHTML = `<h2>${input.value} is ${numValue}</h2>`
+})
