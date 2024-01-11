7e199e7d4dc08151cb8121ea03d49880f6217570
Hestaine <ariffhestaine@gmail.com>
2024-01-11 10:36:55 +0100

    nvnvbn

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ numValue = "null";
 
 input.addEventListener("input", (e) => {
 if (input.value % 2 == 1) {
-    numValue = "0dd"
+    displayValue.classList.remove("green");
+    displayValue.classList.add("red");
+    numValue = "0dd";
 }
 else if (input.value % 2 == 0) {
+    displayValue.classList.add("green");
     numValue = "even";
 }
     displayValue.innerHTML = `<h2>${input.value} is ${numValue}</h2>`
